1a7476b9c9b7670fcbf96b983a5ede620f756697
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-06 09:39:13 +0000

    commented out broken code

--- controller.rb
+++ controller.rb
@@ -7,11 +7,11 @@ get '/films' do
   @films = Film.all
   erb(:'films/index') #sinatra is built to look inside directory views for this file. thats why no filepath
 end
-
-get '/films/:film_id' do
-  @films = Film.all
-  for film in @films
-    @filminfo = display_film_info(param[:film_id].to_i)
-    erb(:'films/film')
-  end
-end
+#
+# get '/films/:film_id' do
+#   @films = Film.all
+#   for film in @films
+#     @filminfo = display_film_info(param[:film_id].to_i)
+#     erb(:'films/film')
+#   end
+# end
